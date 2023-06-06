ca74b1f9f98f96c2a5e9cc02d604c38351d52c72
capriol1 <vittorio.vcaprioli@gmail.com>
2023-06-06 10:00:27 -0300

    v0.5.0.2 Função comprar e alugar automovel

--- Pessoas/Cliente.java
+++ Pessoas/Cliente.java
@@ -7,7 +7,13 @@ public class Cliente extends Pessoa {
     public Cliente(int id, String nome, String cpf, int idade, String telefone, String cep) {
         super(id, nome, cpf, idade, telefone, cep);
     }
+    public void comprarAutomavel(String nome, String telefone, double valor, String modelo, String marca){
 
+    }
+
+    public void alugarAutomavel(String nome,String telefone, double valor, int tempo, String modelo, String marca){
+
+    }
     //Metódo comprar Automoveis.Carro
     //Metódo alugar Automoveis.Carro
 }
