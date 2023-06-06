3f8aea321705e79b7b29ea9489621b48848f6b42
Joao Gabriel <jgstressertrigo@gmail.com>
2023-06-06 10:11:24 -0300

    v0.7.0.2 implementação metodos de registro de vendas e alugueis

--- Registros/RegistroVenda.java
+++ Registros/RegistroVenda.java
@@ -1,13 +1,22 @@
 package Registros;
 
+import Automoveis.Automovel;
 import Pessoas.Cliente;
 import Pessoas.Vendedor;
+import java.util.Date;
 
 public class RegistroVenda{
     private Vendedor vendedor;
     private Cliente cliente;
+    private Date dataVenda;
+    private Automovel automovel;
+    private double valorVenda;
 
-    public void registrarvenda() {
-        //implementar método
+    public static void registrarVenda(Automovel automovel, Vendedor vendedor, Cliente cliente, Date dataVenda, double valorVenda) {
+        automovel = automovel;
+        vendedor = vendedor;
+        cliente = cliente;
+        dataVenda = dataVenda;
+        valorVenda = valorVenda;
     }
 }
