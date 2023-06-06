7cd29e9838876190c9fe9b2fceb3f1b6c12c9612
capriol1 <vittorio.vcaprioli@gmail.com>
2023-06-06 11:13:24 -0300

    v0.9.1.4 Opções do menu principal

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ import Automoveis.Carro;
 import Formulario.FormRefactored;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
@@ -13,5 +14,39 @@ public class main {
                 "           João Gabriel Trigo\n" +
                 "           Vittorio Caprioli");
 
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("***MENU***\n" +
+                "1. Menu Carro \n" +
+                "2. Menu Moto\n" +
+                "3. Menu Caminhão\n" +
+                "4. Menu Cliente\n" +
+                "5. Menu Vendedor\n" +
+                "6. Menu Estoque\n" +
+                "7. Finalizar");
+        int op = teclado.nextInt();
+        switch (op){
+            case 1:
+                // Opções carros
+                break;
+            case 2:
+                // Opções motos
+                break;
+            case 3:
+                // Opções caminhão
+                break;
+            case 4:
+                // opções cliente
+                break;
+            case 5:
+                // opções vendedor
+                break;
+            case 6:
+                // opções estoque
+                break;
+            case 7:
+                break;
+            default:
+                System.out.println("Opção de menu inválida");
+        }
     }
 }
