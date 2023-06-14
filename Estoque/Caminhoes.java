37418e94ccf3628a207ed397eea19f03e0c84d03
Joao Gabriel <jgstressertrigo@gmail.com>
2023-06-14 08:38:06 -0300

    v3.24.1.7 implementação caminhões

---
+++ Estoque/Caminhoes.java
@@ -0,0 +1,34 @@
+package Estoque;
+import Automoveis.Caminhao;
+import Pessoas.Cliente;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Caminhoes implements Serializable{
+    private ArrayList<Caminhao> caminhoes = new ArrayList<>();
+
+    public Caminhoes() {
+        caminhoes = new ArrayList<>();
+    }
+
+    public void addCaminhao(Caminhao caminhoes) {
+        this.caminhoes.add(caminhoes);
+    }
+    public void removeCaminhao(Caminhao caminhoes) {
+        this.caminhoes.remove(caminhoes);
+    }
+
+    public Caminhao findCaminhao(String chassi){
+        Caminhao caminhoes = null;
+        for (Caminhao c : this.caminhoes){
+            if (Objects.equals(c.getChassi(), chassi)){
+                caminhoes = c;
+            }
+        }
+        return caminhoes;
+    }
+
+}
+
