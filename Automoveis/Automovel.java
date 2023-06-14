054d574d36273a49c061e4ec73d0635dcfc7f65c
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2023-06-14 15:53:49 -0300

    v3.26.1.8 Getters e Setters Automovel e mudança de tipo kilomt

--- Automoveis/Automovel.java
+++ Automoveis/Automovel.java
@@ -5,7 +5,7 @@ public abstract class Automovel extends Formulario.FormRefactored {
     private String marca;
     private String modelo;
     private int idade;
-    private long kilomt;
+    private double kilomt;
     private String tipo;
     private String combustivel;
     private int qt_marcha;
@@ -14,7 +14,7 @@ public abstract class Automovel extends Formulario.FormRefactored {
     private int tamanhotanque;
     private double valor;
 
-    public Automovel(String marca, String modelo, int idade, long kilomt, String tipo, String combustivel, int qt_marcha, String cor, String chassi, int tamanhotanque, double valor) throws HeadlessException {
+    public Automovel(String marca, String modelo, int idade, double kilomt, String tipo, String combustivel, int qt_marcha, String cor, String chassi, int tamanhotanque, double valor) throws HeadlessException {
         this.marca = marca;
         this.modelo = modelo;
         this.idade = idade;
@@ -48,6 +48,78 @@ public abstract class Automovel extends Formulario.FormRefactored {
 
     public void setValor(double valor) {
         this.valor = valor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getKilomt() {
+        return kilomt;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public int getQt_marcha() {
+        return qt_marcha;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public int getTamanhotanque() {
+        return tamanhotanque;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setKilomt(double kilomt) {
+        this.kilomt = kilomt;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public void setQt_marcha(int qt_marcha) {
+        this.qt_marcha = qt_marcha;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public void setTamanhotanque(int tamanhotanque) {
+        this.tamanhotanque = tamanhotanque;
     }
 
     public void vender(Automovel automovel){
